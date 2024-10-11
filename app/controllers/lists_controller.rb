e5afd37641c10a5ab8726b4812987f93c8a357e8
EC2 Default User <ec2-user@ip-172-31-16-107.ap-northeast-1.compute.internal>
2024-10-11 10:54:58 +0000

    [Finish]3-7

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -30,14 +30,20 @@ class ListsController < ApplicationController
 
 #編集画面
   def edit
-    @list = List.find(params[:id])
+    @list = List.find(params[:id]) # データ（レコード）を1件取得
     
   end
   
   def update
-    list =List.find(params[:id])
+    list =List.find(params[:id]) # データ（レコード）を1件取得
     list.update(list_params)
-    redirect_to list_path(list.id)
+    redirect_to list_path(list.id)  # 詳細画面(show.hetm.erb)へリダイレクト 
+  end
+  
+  def destroy
+    list = List.find(params[:id]) # データ（レコード）を1件取得
+    list.destroy # データ（レコード）を削除
+    redirect_to '/lists'  # 投稿一覧画面へリダイレクト 
   end
   
 # ここから下はこのcontrollerの中でしか呼び出せません 
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,21 @@
 Rails.application.routes.draw do
    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
-  get 'lists/new' #'lists'コントローラーの'new'アクションを呼び出す
-  get '/top' => 'homes#top' #GETリクエストが `/top` というパスに対して送信された場合に、`homes`コントローラーの`top`アクションを呼び出す
-  post 'lists' => 'lists#create' #lists_controller.rb 内変数 create 新しいデータの作成
-  get 'lists' => 'lists#index'  #'lists'コントローラーの'index'アクションを呼び出す
+#   get 'lists/new' #'lists'コントローラーの'new'アクションを呼び出す
+  get 'top' => 'homes#top' #GETリクエストが `/top` というパスに対して送信された場合に、`homes`コントローラーの`top`アクションを呼び出す
+  resources :lists
+  
+#   post 'lists' => 'lists#create' #lists_controller.rb 内変数 create 新しいデータの作成
+#   get 'lists' => 'lists#index'  #'lists'コントローラーの'index'アクションを呼び出す
   
   #どの投稿データを表示するかを判別するために、投稿データのidもURLに含めます。 
   #たとえば、URLが/lists/1の場合、params[:id] と記述すると、id=1を取り出せます。
-  get 'lists/:id' => 'lists#show', as: 'list'#getリクエストがlists/:idというパスに送信された場合'lists'コントローラーの'show'アクションを呼び出す
+#   get 'lists/:id' => 'lists#show', as: 'list'#getリクエストがlists/:idというパスに送信された場合'lists'コントローラーの'show'アクションを呼び出す
   
-  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+#   get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'#getリクエストがlists/:idというパスに送信された場合'lists'コントローラーの'edit'アクションを呼び出す
 
-  patch 'lists/:id' => 'lists#update', as: 'update_list'
+#   patch 'lists/:id' => 'lists#update', as: 'update_list'#patchリクエストがlists/:idというパスに送信された場合'lists'コントローラーの'update'アクションを呼び出す
+  
+#   delete 'lists/:id' => 'lists#destroy', as: 'destroy_list'#deleteリクエストがlists/:idというパスに送信された場合'lists'コントローラーの'destroy'アクションを呼び出す
   
 end
